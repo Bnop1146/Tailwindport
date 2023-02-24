f2b10a0ad0dd03491b44e9ca12b2fbdd1ba59a33
Bnop1146 <79976803+Bnop1146@users.noreply.github.com>
2023-02-24 04:02:59 +0100

    Færdig med Tailwind Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // ...
+        'primary': {
+          secondary: '#FF9705',
+          main: '#001023',
+          darktext: '#212427',
+          Git: '#333',
+          In: '#0077b5',
+          
+          
+          
+        },
+
+    },
   },
   plugins: [],
+}
+  // ...
 }
